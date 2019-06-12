70ad58fab1dacf64655bb9123de4166f78326987
Anna Quinlan <anna@tidepool.org>
2019-06-12 16:06:39 -0700

    Add momentum testcase

--- LoopKitTests/GlucoseMathTests.py
+++ LoopKitTests/GlucoseMathTests.py
@@ -1,11 +1,15 @@
 
+import sys, os, json
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
 from LoopKitTests import loadFixture, dateFormatter, HKQuantity
-from GlucoseEffect import GlucoseEffect
+from LoopKit.GlucoseEffect import GlucoseEffect
 from GlucoseEffectVelocity import GlucoseEffectVelocity
-import json
+from LoopKit.GlucoseKit.GlucoseMath import linearMomentumEffect
 
 class GlucoseFixtureValue:
-    def __init__ (self, startDate, quantity, isDisplayOnly, provenanceIdentifier, unit):
+
+    def __init__ (self, startDate, quantity, isDisplayOnly, provenanceIdentifier):
         self.startDate = startDate
         self.quantity = quantity
         self.isDisplayOnly = isDisplayOnly
@@ -23,20 +27,19 @@ class GlucoseFixtureValue:
 
 def runTestCases ():
 
-    # have to specify folder if the file is within one
     def loadInputFixture (resourceName):
-        fixture = loadFixture (resourceName)
+        fixture = loadFixture (resourceName, ".json")
 
         def glucoseFixtureMaker (dict):
             return GlucoseFixtureValue (dateFormatter (dict.get("date")), 
                                         HKQuantity (dict.get("unit"), dict.get("amount")), 
                                         dict.get("display_only") or False, 
-                                        dict.get("provenance_identifier"), 
+                                        dict.get("provenance_identifier") )
         return map(glucoseFixtureMaker, fixture)
 
 
     def loadOutputFixture (resourceName):
-        fixture = loadFixture (resourceName)
+        fixture = loadFixture (resourceName, ".json")
 
         def glucoseEffectMaker (dict):
             return GlucoseEffect(dateFormatter (dict.get("date")), 
@@ -47,14 +50,50 @@ def runTestCases ():
 
 
     def loadEffectVelocityFixture (resourceName):
-        fixture = loadFixture(resourceName)
+        fixture = loadFixture(resourceName, ".json")
 
         def glucoseEffectVelocityMaker (dict):
             return GlucoseEffectVelocity(dateFormatter (dict.get("startDate")), 
                                          dateFormatter (dict.get("endDate")), 
                                          HKQuantity (dict.get("unit"), dict.get("value") ) )
-                                         
 
+        return map (glucoseEffectVelocityMaker, fixture)
+
+    '''
+    func testMomentumEffectForBouncingGlucose() {
+        let input = loadInputFixture("momentum_effect_bouncing_glucose_input")
+        let output = loadOutputFixture("momentum_effect_bouncing_glucose_output")
+
+        let effects = input.linearMomentumEffect()
+        let unit = HKUnit.milligramsPerDeciliter
+
+        XCTAssertEqual(output.count, effects.count)
+
+        for (expected, calculated) in zip(output, effects) {
+            XCTAssertEqual(expected.startDate, calculated.startDate)
+            XCTAssertEqual(expected.quantity.doubleValue(for: unit), calculated.quantity.doubleValue(for: unit), accuracy: Double(Float.ulpOfOne))
+        }
+    }
+                                         
+    '''
+
+    def testMomentumEffectForBouncingGlucose():
+        inputt = loadInputFixture("momentum_effect_bouncing_glucose_input")
+        output = loadOutputFixture ("momentum_effect_bouncing_glucose_output")
+
+        effects = linearMomentumEffect (inputt)
+        unit = "mg/dL"
+
+        if len(output) != len (effects):
+            print("Test failed, expected output is length", len(output), "but output from program is",len (effects))
+        for (expected, calculated) in zip(output, effects):
+            if expected.startDate != calculated.startDate:
+                print ("Test failed because",expected.startDate,"!=",calculated.startDate)
+        #! DIDN'T INCLUDE LAST TEST AND NEED TO DO SO
+        print ("Test passed!!!")
+    testMomentumEffectForBouncingGlucose()
+
+runTestCases()
 
 
 

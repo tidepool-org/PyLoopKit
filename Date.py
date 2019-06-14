a720e6b95ee0406b6c6237724388b2469ea6e3fc
Anna Quinlan <anna@tidepool.org>
2019-06-14 13:47:39 -0700

    Add change parameter passed into function from object to a time value

--- Date.py
+++ Date.py
@@ -13,32 +13,38 @@ import math
 import datetime
 
 
-# time_interval_since_reference_date returns the number of seconds
-# since January, 1st, 2001: 12:00 am (mid night)
-def time_interval_since_reference_date(obj):
+# input - datetime object
+# output - returns the number of seconds
+#          since January, 1st, 2001: 12:00 am (mid night)
+def time_interval_since_reference_date(actual_time):
     ref_time = date_formatter("2001-01-01T00:00:00")
-    actual_time = obj.start_date
     dif = abs(actual_time - ref_time)
     return dif
 
 
-def date_floored_to_time_interval(obj, interval):
+# input - two datetime objects
+# output - the seconds between two times (with a sign)
+def time_interval_since(date_1, date_2):
+    return (date_2 - date_1).total_seconds()
+
+
+def date_floored_to_time_interval(time, interval):
     if interval == 0:
-        return obj
+        return time
     ref_time = date_formatter("2001-01-01T00:00:00")
     # this assumes the interval is in mins
-    floored_delta = (math.floor(time_interval_since_reference_date(obj)
+    floored_delta = (math.floor(time_interval_since_reference_date(time)
                      / interval / 60) * interval * 60)
 
     return ref_time + datetime.timedelta(seconds=floored_delta)
 
 
-def date_ceiled_to_time_interval(obj, interval):
+def date_ceiled_to_time_interval(time, interval):
     if interval == 0:
-        return obj
+        return time
     ref_time = date_formatter("2001-01-01T00:00:00")
     # this assumes the interval is in mins
-    ceiled_delta = (math.ceil(time_interval_since_reference_date(obj)
+    ceiled_delta = (math.ceil(time_interval_since_reference_date(time)
                     / interval / 60) * interval * 60)
 
     return ref_time + datetime.timedelta(seconds=ceiled_delta)

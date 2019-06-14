1ce68f6f9a09ea31bb79508df4c6b1f81fa210f5
Anna Quinlan <anna@tidepool.org>
2019-06-14 14:17:49 -0700

    Create ability to simulate a date range over an interval

---
+++ LoopMath.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 14 10:40:48 2019
+
+@author: annaquinlan
+
+Github URL: https://github.com/tidepool-org/LoopKit/blob/
+            57a9f2ba65ae3765ef7baafe66b883e654e08391/LoopKit/LoopMath.swift
+"""
+from Date import date_floored_to_time_interval, date_ceiled_to_time_interval
+from datetime import timedelta
+
+
+def simulation_date_range_for_samples(samples, duration, delta, start=None,
+                                      end=None, delay=0):
+    if len(samples) <= 0:
+        return
+    if start is not None and end is not None:
+        return(date_floored_to_time_interval(start, delta),
+               date_ceiled_to_time_interval(end, delta))
+    else:
+        min_date = samples[0].start_date
+        max_date = min_date
+        for sample in samples:
+            if sample.start_date < min_date:
+                min_date = sample.start_date
+            if sample.end_date > max_date:
+                max_date = sample.end_date
+        return (date_floored_to_time_interval(start or min_date, delta),
+                date_ceiled_to_time_interval(end or max_date +
+                                             timedelta(minutes=delay), delta))

f6b525eb2fea4f3539ac5030895cd7381e656d33
Anna Quinlan <anna@tidepool.org>
2019-06-17 11:38:05 -0700

    Fix case

--- test/test_ns_date.py
+++ test/test_ns_date.py
@@ -7,7 +7,7 @@ Created on Sun Jun 16 15:41:18 2019
 """
 import unittest
 from datetime import datetime, timedelta
-from Date import (date_floored_to_time_interval, date_ceiled_to_time_interval,
+from date import (date_floored_to_time_interval, date_ceiled_to_time_interval,
                   time_interval_since_reference_date, time_interval_since)
 
 

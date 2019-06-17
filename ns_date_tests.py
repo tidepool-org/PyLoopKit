db0d3c29e10972fa747e73a14d968313167fe953
Anna Quinlan <anna@tidepool.org>
2019-06-16 17:04:38 -0700

    Add more time tests
    Testing for time_interval_since_reference_date and time_interval_since

--- ns_date_tests.py
+++ ns_date_tests.py
@@ -10,12 +10,38 @@ from date import (date_floored_to_time_interval, date_ceiled_to_time_interval,
                   time_interval_since_reference_date, time_interval_since)
 
 
+"""
+  Checks if two times are equal
+- input: two datetime objects
+- output: string stating that test is passed or failed
+"""
+
+
 def is_test_passed(time_1, time_2):
     if time_1 == time_2:
         return "Test passed"
     else:
         return "Test failed;", time_1.strftime('%m/%d/%y %H:%M:%S.%f'),\
                "!=", time_2.strftime('%m/%d/%y %H:%M:%S.%f')
+
+
+"""
+  Checks if two numbers are equal
+- input: two datetime objects
+- output: string stating that test is passed or failed
+"""
+
+
+def is_test_passed_nums(num_1, num_2):
+    if num_1 == num_2:
+        return "Test passed"
+    else:
+        return "Test failed;", num_1, "!=", num_2
+
+
+"""
+  Tests for date_ceiled_to_time_interval
+"""
 
 
 def test_date_ceiled_to_interval():
@@ -44,6 +70,11 @@ def test_date_ceiled_to_interval():
           "\n")
 
 
+"""
+  Tests for date_floored_to_time_interval
+"""
+
+
 def test_date_floored_to_interval():
     calendar = datetime.now()
     five_01 = calendar.replace(hour=5, minute=0, second=1, microsecond=0)
@@ -60,6 +91,59 @@ def test_date_floored_to_interval():
     print(is_test_passed(five_01, date_floored_to_time_interval(five_01, 0)),
           "\n")
 
+"""
+  Tests for time_interval_since_reference_date
+"""
+
+
+def test_time_interval_since_reference_date():
+    ref_date = datetime(2001, 1, 1, 0, 0, 0)
+    print(is_test_passed_nums(0, time_interval_since_reference_date(ref_date)))
+    
+    print(is_test_passed_nums(371,
+                              time_interval_since_reference_date(ref_date +
+                                                                 timedelta(seconds=371))))
+    print(is_test_passed_nums(12345,
+                              time_interval_since_reference_date(ref_date +
+                                                                 timedelta(seconds=12345))))
+    print(is_test_passed_nums(200,
+                              time_interval_since_reference_date(ref_date +
+                                                                 timedelta(seconds=-200))))
+    print(is_test_passed_nums(86400,
+                              time_interval_since_reference_date(ref_date +
+                                                                 timedelta(seconds=-86400))))
+    print(is_test_passed_nums(86400,
+                              time_interval_since_reference_date(ref_date +
+                                                                 timedelta(seconds=86400)))
+          , "\n")
+
+def test_time_interval_since():
+    date = datetime.now()
+    print(is_test_passed_nums(0, time_interval_since(date, date)))
+    
+    print(is_test_passed_nums(-371,
+                              time_interval_since(date, date +
+                                                  timedelta(seconds=371))))
+    print(is_test_passed_nums(123456,
+                              time_interval_since(date, date +
+                                                  timedelta(seconds=-123456))))
+    print(is_test_passed_nums(-200,
+                              time_interval_since(date, date +
+                                                  timedelta(seconds=200))))
+
+    print(is_test_passed_nums(200,
+                              time_interval_since(date, date +
+                                                  timedelta(seconds=-200))))
+    print(is_test_passed_nums(86400,
+                              time_interval_since(date, date +
+                                                  timedelta(seconds=-86400))))
+    print(is_test_passed_nums(-86400,
+                              time_interval_since(date, date +
+                                                  timedelta(seconds=86400))),
+          "\n")
+
 
 test_date_ceiled_to_interval()
 test_date_floored_to_interval()
+test_time_interval_since_reference_date()
+test_time_interval_since()

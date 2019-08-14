6bd2307964b5b65ecc1bb5a170b8dc4d5753e68a
Anna Quinlan <anna@tidepool.org>
2019-08-14 11:57:35 -0700

    Plot all the different components of the prediction

--- example.py
+++ example.py
@@ -11,6 +11,7 @@ import datetime
 from generate_graphs import plot_graph, plot_loop_inspired_glucose_graph
 from insulin_math import find_ratio_at_time
 from loop_kit_tests import find_root_path
+from loop_math import predict_glucose
 from pyloop_parser import parse_report_and_run
 
 # find the path to the file in the repo
@@ -19,6 +20,47 @@ path = find_root_path(name.split(".")[0], "." + name.split(".")[1])
 
 # run the Loop algorithm with the issue report data
 recommendations = parse_report_and_run(path, name)
+
+# generate separate glucose predictions using each effect individually
+starting_date = recommendations.get("input_data").get("glucose_dates")[-1]
+starting_glucose = recommendations.get("input_data").get("glucose_values")[-1]
+
+(momentum_predicted_glucose_dates,
+ momentum_predicted_glucose_values
+ ) = predict_glucose(
+     starting_date, starting_glucose,
+     recommendations.get("momentum_effect_dates"),
+     recommendations.get("momentum_effect_values")
+     )
+
+(insulin_predicted_glucose_dates,
+ insulin_predicted_glucose_values
+ ) = predict_glucose(
+     starting_date, starting_glucose,
+     recommendations.get("insulin_effect_dates"),
+     recommendations.get("insulin_effect_values")
+     )
+
+(carb_predicted_glucose_dates,
+ carb_predicted_glucose_values
+ ) = predict_glucose(
+     starting_date, starting_glucose,
+     recommendations.get("carb_effect_dates"),
+     recommendations.get("carb_effect_values")
+     )
+
+if recommendations.get("retrospective_effect_dates"):
+    (retrospective_predicted_glucose_dates,
+     retrospective_predicted_glucose_values
+     ) = predict_glucose(
+         starting_date, starting_glucose,
+         recommendations.get("retrospective_effect_dates"),
+         recommendations.get("retrospective_effect_values")
+         )
+else:
+    (retrospective_predicted_glucose_dates,
+     retrospective_predicted_glucose_values
+     ) = ([], [])
 
 
 # save dictionary as json file
@@ -40,6 +82,7 @@ with open(name.split(".")[0] + "-output.json", "w") as f:
 
 # visualize some of that data
 
+# plot insulin effects
 plot_graph(
     recommendations.get("insulin_effect_dates"),
     recommendations.get("insulin_effect_values"),
@@ -47,34 +90,53 @@ plot_graph(
     grid=True,
     )
 
+# plot counteraction effects
 plot_graph(
-    recommendations.get("counteraction_effect_start_times")[-len(recommendations.get("insulin_effect_dates")):],
-    recommendations.get("counteraction_effect_values")[-len(recommendations.get("insulin_effect_dates")):],
+    recommendations.get("counteraction_effect_start_times")[
+        # trim to a reasonable length so the effects aren't too close together
+        -len(recommendations.get("insulin_effect_dates")):
+    ],
+    recommendations.get("counteraction_effect_values")[
+        # trim to a reasonable length so the effects aren't too close together
+        -len(recommendations.get("insulin_effect_dates")):
+    ],
     title="Counteraction Effects",
     fill_color="#f09a37",
     grid=True
     )
 
-plot_graph(
-    recommendations.get("carb_effect_dates"),
-    recommendations.get("carb_effect_values"),
-    title="Carb Effect",
-    line_color="#5FCB49",
-    grid=True
-    )
+# only plot carb effects if we have that data
+if recommendations.get("carb_effect_values"):
+    plot_graph(
+        recommendations.get("carb_effect_dates"),
+        recommendations.get("carb_effect_values"),
+        title="Carb Effect",
+        line_color="#5FCB49",
+        grid=True
+        )
 
-plot_graph(
-    recommendations.get("cob_timeline_dates"),
-    recommendations.get("cob_timeline_values"),
-    title="Carbs on Board",
-    line_color="#5FCB49", fill_color="#63ed47"
-    )
+# only plot the carbs on board over time if we have that data
+if recommendations.get("cob_timeline_values"):
+    plot_graph(
+        recommendations.get("cob_timeline_dates"),
+        recommendations.get("cob_timeline_values"),
+        title="Carbs on Board",
+        line_color="#5FCB49", fill_color="#63ed47"
+        )
 
 inputs = recommendations.get("input_data")
 
 plot_loop_inspired_glucose_graph(
     recommendations.get("predicted_glucose_dates"),
     recommendations.get("predicted_glucose_values"),
+    momentum_predicted_glucose_dates,
+    momentum_predicted_glucose_values,
+    insulin_predicted_glucose_dates,
+    insulin_predicted_glucose_values,
+    carb_predicted_glucose_dates,
+    carb_predicted_glucose_values,
+    retrospective_predicted_glucose_dates,
+    retrospective_predicted_glucose_values,
     title="Predicted Glucose",
     line_color="#5ac6fa",
     grid=True,

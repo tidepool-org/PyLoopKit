3387c2bd94e56711ccbbbc28c6791604aec873b2
Ed Nykaza <edward.t.nykaza@gmail.com>
2019-09-13 07:41:35 -0500

    make prepare figure function

--- view_scenarios.py
+++ view_scenarios.py
@@ -9,10 +9,8 @@ import os
 import datetime
 import pandas as pd
 import numpy as np
-from input_data_tools import input_table_to_dict, dict_inputs_to_dataframes
-from loop_data_manager import update
+from input_data_tools import dict_inputs_to_dataframes
 import plotly.graph_objs as go
-from plotly.offline import plot
 
 
 # %% functions
@@ -30,7 +28,7 @@ def get_bg_colors(df):
     return df
 
 
-def prepare_bg(df):
+def prepare_bg(df, current_time):
     df = get_bg_colors(df)
 
     df_trace = go.Scattergl(
@@ -122,7 +120,7 @@ def downsample(df, current_time, freq="5min"):
     return df
 
 
-def prepare_basal(basal_rates, df_dose, contig_ts):
+def prepare_basal(basal_rates, df_dose, contig_ts, current_time):
     unique_dose_types = df_dose["type"].unique()
     df = pd.merge(
         contig_ts,
@@ -278,7 +276,7 @@ def prepare_carbs(df_events, df_ratios, continguous_ts):
     return carb_df, df_trace
 
 
-def prepare_target_range(df_target_range, continguous_ts):
+def prepare_target_range(df_target_range, continguous_ts, current_time):
     df = pd.merge(
         continguous_ts,
         df_target_range,
@@ -328,7 +326,7 @@ def prepare_target_range(df_target_range, continguous_ts):
     return df, trace_top, trace_bottom
 
 
-def prepare_insulin_axis(basal, bolus, carbs):
+def prepare_insulin_axis(basal, bolus, carbs, current_time):
 
     axis = dict(
         domain=[0, 0.3],
@@ -408,7 +406,7 @@ def prepare_loop_prediction(predicted_bg_dates, predicted_bg_values):
     return bg_prediction_trace
 
 
-def prepare_loop_temp_basal(current_time, recommended_temp_basal):
+def prepare_loop_temp_basal(basal, current_time, recommended_temp_basal):
     if recommended_temp_basal is not None:
         rec_temp_basal_rate = recommended_temp_basal[0]
         rec_temp_basal_duration = recommended_temp_basal[1]
@@ -438,7 +436,7 @@ def prepare_loop_temp_basal(current_time, recommended_temp_basal):
     return rec_basal_trace
 
 
-def prepare_loop_bolus(recommended_bolus):
+def prepare_loop_bolus(recommended_bolus, current_time):
     recommended_bolus_trace = go.Bar(
         name="bolus recommended",
         x=[current_time],
@@ -454,7 +452,7 @@ def prepare_loop_bolus(recommended_bolus):
     return recommended_bolus_trace
 
 
-def prepare_suspend(suspend_threshold):
+def prepare_suspend(suspend_threshold, current_time):
     df_trace = go.Scatter(
         name="suspend threshold = {}".format(suspend_threshold),
         mode='lines',
@@ -476,93 +474,112 @@ def prepare_suspend(suspend_threshold):
     return df_trace
 
 
-# %% view the scenario
-# load in example scenario files
-cutom_scenario_files = [
-    "custom-scenario-table-template-simple.csv",
-    "custom-scenario-table-template-complex.csv",
-    "custom-scenario-table-example-3.csv",
-]
-path = os.path.join(".", "example_files")
-table_path_name = os.path.join(path, cutom_scenario_files[2])
-custom_table_df = pd.read_csv(table_path_name, index_col=0)
-inputs = input_table_to_dict(custom_table_df)
-# first make sure that the scenario runs
-# getting no errors on run is a good sign
-loop_output = update(inputs)
+def make_scenario_figure(loop_output):
+    inputs = loop_output["input_data"]
 
-# convert dict_inputs_to_dataframes
-(
- basal_rates, carb_events, carb_ratios, dose_events, bg_df,
- df_last_temporary_basal, df_misc, df_sensitivity_ratio,
- df_settings, df_target_range
-) = dict_inputs_to_dataframes(inputs)
+    # convert dict_inputs_to_dataframes
+    (
+     basal_rates, carb_events, carb_ratios, dose_events, blood_glucose,
+     df_last_temporary_basal, df_misc, df_sensitivity_ratio,
+     df_settings, df_target_range
+    ) = dict_inputs_to_dataframes(inputs)
 
-current_time = inputs.get("time_to_calculate_at")
+    current_time = inputs.get("time_to_calculate_at")
+
+    # %% blood glucose data
+    bg_df, bg_trace, bg_axis, bg_annotation = (
+        prepare_bg(blood_glucose, current_time)
+    )
+
+    # create a contiguous time series for the other data types
+    date_min = bg_df["glucose_dates"].min() - datetime.timedelta(days=1)
+    date_max = current_time + datetime.timedelta(days=1)
+    continguous_ts = create_contiguous_ts(date_min, date_max)
+
+    # target range
+    target_range, target_trace_top, target_trace_bottom = (
+        prepare_target_range(df_target_range, continguous_ts, current_time)
+    )
+
+    # suspend threshold
+    suspend_threshold = inputs["settings_dictionary"]["suspend_threshold"]
+    suspend_trace = prepare_suspend(suspend_threshold, current_time)
+
+    # %% insulin and carb data
+    dose_events["type"] = (
+        dose_events["dose_types"].apply(convert_times_and_types)
+    )
+
+    # bolus data
+    bolus, bolus_trace = prepare_bolus(dose_events)
+
+    # basal data
+    basal, sbr, basal_delivered_trace, scheduled_basal_trace,  = (
+        prepare_basal(basal_rates, dose_events, continguous_ts, current_time)
+    )
+
+    # carb data (cir and carb events)
+    carbs, carb_trace = prepare_carbs(carb_events, carb_ratios, continguous_ts)
+
+    # prepare insulin axis
+    insulin_axis, insulin_annotation = (
+        prepare_insulin_axis(basal, bolus, carbs, current_time)
+    )
+
+    # %% add loop prediction, temp basal, and bolus recommendation
+    # loop prediction
+    predicted_glucose_dates = loop_output.get("predicted_glucose_dates")
+    predicted_glucose_values = loop_output.get("predicted_glucose_values")
+    loop_prediction_trace = prepare_loop_prediction(
+        predicted_glucose_dates, predicted_glucose_values
+    )
+
+    # recommended temp basal
+    loop_temp_basal = loop_output.get("recommended_temp_basal")
+    loop_basal_trace = (
+        prepare_loop_temp_basal(basal, current_time, loop_temp_basal)
+    )
+
+    # recommended bolus
+    loop_rec_bolus = loop_output.get("recommended_bolus")[0]
+    loop_bolus_trace = prepare_loop_bolus(loop_rec_bolus, current_time)
+
+    # %% make figure
+    fig_layout = prepare_layout(
+        current_time, bg_axis, insulin_axis, bg_annotation, insulin_annotation
+    )
+
+    traces = [
+        basal_delivered_trace, scheduled_basal_trace, bolus_trace, carb_trace,
+        loop_bolus_trace, loop_basal_trace, loop_prediction_trace,
+        suspend_trace, target_trace_top, target_trace_bottom, bg_trace
+    ]
+    fig = go.Figure(data=traces, layout=fig_layout)
+
+    return fig
 
 
-# %% blood glucose data
-bg_df, bg_trace, bg_axis, bg_annotation = prepare_bg(bg_df.copy())
+# %% view example scenario(s)
+def view_example():
+    from input_data_tools import input_table_to_dict
+    from loop_data_manager import update
+    from plotly.offline import plot
 
-# create a contiguous time series for the other data types
-date_min = bg_df["glucose_dates"].min() - datetime.timedelta(days=1)
-date_max = current_time + datetime.timedelta(days=1)
-continguous_ts = create_contiguous_ts(date_min, date_max)
-
-# target range
-target_range, target_trace_top, target_trace_bottom = (
-    prepare_target_range(df_target_range, continguous_ts)
-)
-
-# suspend threshold
-suspend_threshold = inputs["settings_dictionary"]["suspend_threshold"]
-suspend_trace = prepare_suspend(suspend_threshold)
-
-
-# %% insulin and carb data
-dose_events["type"] = dose_events["dose_types"].apply(convert_times_and_types)
-
-# bolus data
-bolus, bolus_trace = prepare_bolus(dose_events)
-
-# basal data
-basal, sbr, basal_delivered_trace, scheduled_basal_trace,  = (
-    prepare_basal(basal_rates, dose_events, continguous_ts)
-)
-
-# carb data (cir and carb events)
-carbs, carb_trace = prepare_carbs(carb_events, carb_ratios, continguous_ts)
-
-# prepare insulin axis
-insulin_axis, insulin_annotation = prepare_insulin_axis(basal, bolus, carbs)
+    # load in example scenario files
+    cutom_scenario_files = [
+        "custom-scenario-table-template-simple.csv",
+        "custom-scenario-table-template-complex.csv",
+        "custom-scenario-table-example-3.csv",
+        "hypothetical-scenario-1.csv"
+    ]
+    path = os.path.join(".", "example_files")
+    table_path_name = os.path.join(path, cutom_scenario_files[0])
+    custom_table_df = pd.read_csv(table_path_name, index_col=0)
+    inputs_from_file = input_table_to_dict(custom_table_df)
+    loop_algorithm_output = update(inputs_from_file)
+    plotly_fig = make_scenario_figure(loop_algorithm_output)
+    plot(plotly_fig, filename=table_path_name+".html")
 
 
-# %% add loop prediction, temp basal, and bolus recommendation
-# loop prediction
-predicted_glucose_dates = loop_output.get("predicted_glucose_dates")
-predicted_glucose_values = loop_output.get("predicted_glucose_values")
-loop_prediction_trace = (
-    prepare_loop_prediction(predicted_glucose_dates, predicted_glucose_values)
-)
-
-# recommended temp basal
-loop_temp_basal = loop_output.get("recommended_temp_basal")
-loop_basal_trace = prepare_loop_temp_basal(current_time, loop_temp_basal)
-
-# recommended bolus
-loop_rec_bolus = loop_output.get("recommended_bolus")[0]
-loop_bolus_trace = prepare_loop_bolus(loop_rec_bolus)
-
-
-# %% make figure
-fig_layout = prepare_layout(
-    current_time, bg_axis, insulin_axis, bg_annotation, insulin_annotation
-)
-
-traces = [
-    basal_delivered_trace, scheduled_basal_trace, bolus_trace, carb_trace,
-    loop_bolus_trace, loop_basal_trace, loop_prediction_trace, suspend_trace,
-    target_trace_top, target_trace_bottom, bg_trace
-]
-fig = go.Figure(data=traces, layout=fig_layout)
-plot(fig)
+if __name__ == "__main__":
+    view_example()

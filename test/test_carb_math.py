6169445b615d18b5cf48d854f33960d6135463de
russ <russ@tidepool.org>
2019-10-29 18:13:33 -0500

    fixing test

--- test/test_carb_math.py
+++ test/test_carb_math.py
@@ -12,7 +12,7 @@ Github URL: https://github.com/tidepool-org/LoopKit/blob/
 import unittest
 from datetime import datetime, time, timedelta
 
-from loop_kit_tests import load_fixture
+from .loop_kit_tests import load_fixture
 from pyloopkit.carb_math import (map_, carb_glucose_effects, carbs_on_board,
                                  dynamic_carbs_on_board, dynamic_glucose_effects)
 

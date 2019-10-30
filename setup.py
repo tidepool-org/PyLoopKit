74069fa1090bc39c546b43c9d9485f61715d6d50
russ <russ@tidepool.org>
2019-10-29 19:00:27 -0500

    added travis, LICENSE and move fixtures into tests

--- setup.py
+++ setup.py
@@ -1,22 +1,37 @@
-import setuptools
+from setuptools import setup, find_packages
+import sys
+
+
+if sys.version_info < (3, 7):
+    sys.exit("Sorry, Python < 3.7 is not supported")
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+version_string = "v0.0.1"
+
+setup(
     name="pyloopkit",
-    version="0.0.1",
+    version=version_string,
     author="Tidepool",
     author_email="ed@tidepool.org",
     description="Python implementation of the Loop algorithm",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/tidepool-org/PyLoopKit",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD-2-Clause',
+        'Programming Language :: Python :: 3.7',
     ],
+    install_requires=[
+          'numpy==1.16.4',
+          'pandas==0.24.2',
+          'tensorflow==2.0.0-beta1',
+          'plotly==4.1.0',
+          'matplotlib==3.1.1',
+      ],
     python_requires='>=3.6',
 )

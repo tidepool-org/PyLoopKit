18d57632f6454a63919b1e45a82496ac9ab4eb4b
Ed Nykaza <edward.t.nykaza@gmail.com>
2020-05-07 08:50:11 -0500

    add setup file

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import sys
+
+
+if sys.version_info < (3, 6):
+    sys.exit("Sorry, Python < 3.6 is not supported")
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+version_string = "v0.1.0-dev"
+
+setup(
+    name="pyloopkit",
+    version=version_string,
+    author="Tidepool",
+    author_email="ed@tidepool.org",
+    description="Python implementation of the Loop algorithm",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tidepool-org/PyLoopKit",
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.6',
+    ],
+    install_requires=[
+          'numpy==1.16.4',
+          'backports-datetime-fromisoformat==1.0.0',
+      ],
+    python_requires='>=3.6',
+)
